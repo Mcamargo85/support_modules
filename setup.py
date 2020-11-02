c02e04125f1d0f464e1af3f2c3212c6e359bb01b
Mcamargo85 <38304139+Mcamargo85@users.noreply.github.com>
2020-11-02 17:24:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='support_module',
+setup(name='support_modules',
     version='1.0.0',
     description='support module with utility functions',
     author='Manuel Camargo',
-    url='https://github.com/Mcamargo85/support_module',
+    url='https://github.com/Mcamargo85/support_modules',
     packages=['utils'],
     install_requires=[
         'numpy',
