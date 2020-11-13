d1b0c9c3744a24c9c225ca706382cb1eca02a59a
Mcamargo85 <38304139+Mcamargo85@users.noreply.github.com>
2020-11-13 12:09:15 +0200

    log reader inclusion

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='support_modules',
-    version='1.0.0',
+    version='1.0.1',
     description='support module with utility functions',
     author='Manuel Camargo',
     url='https://github.com/Mcamargo85/support_modules',
-    packages=['utils'],
+    packages=['utils', 'readers'],
     install_requires=[
         'numpy',
-        'networkx'
-    ]
+        'networkx',
+        'pm4py',
+        'pandas']
 )
