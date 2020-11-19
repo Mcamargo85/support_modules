8039bc121276fb0dfbd0c2e92c4df015e0695dca
Mcamargo85 <38304139+Mcamargo85@users.noreply.github.com>
2020-11-19 18:56:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='support_modules',
-    version='1.0.2',
+    version='1.0.3',
     description='support module with utility functions',
     author='Manuel Camargo',
     url='https://github.com/Mcamargo85/support_modules',
-    packages=['utils', 'readers'],
+    packages=['utils', 'readers', 'tests'],
     install_requires=[
         'numpy',
         'networkx',
