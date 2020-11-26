ceb6bad70719bd52186d939d5c5b569cd9e1ee53
Manuel Camargo <manuel.camargo@ut.ee>
2020-11-26 23:33:53 +0200

    Log split addition

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(name='support_modules',
-    version='1.0.3',
-    description='support module with utility functions',
-    author='Manuel Camargo',
-    url='https://github.com/Mcamargo85/support_modules',
-    packages=['utils', 'readers', 'tests'],
-    install_requires=[
+      version='1.0.4',
+      description='support module with utility functions',
+      author='Manuel Camargo',
+      url='https://github.com/Mcamargo85/support_modules',
+      packages=['utils', 'readers', 'tests'],
+      install_requires=[
         'numpy',
         'networkx',
         'pm4py',
         'pandas',
         'matplotlib']
-)
+      )
