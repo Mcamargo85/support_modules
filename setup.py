e8ddb58a8fb4765997cb86628773753f8c2735d3
Mcamargo85 <38304139+Mcamargo85@users.noreply.github.com>
2020-12-07 16:06:58 +0200

    Similarity Evaluator module

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(name='support_modules',
-      version='1.0.4',
+      version='1.0.5',
       description='support module with utility functions',
       author='Manuel Camargo',
       url='https://github.com/Mcamargo85/support_modules',
-      packages=['utils', 'readers', 'tests'],
+      packages=['utils', 'readers', 'tests', 'analyzers'],
       install_requires=[
         'numpy',
         'networkx',
         'pm4py',
         'pandas',
-        'matplotlib']
+        'matplotlib',
+        'jellyfish',
+        'scipy']
       )
