a33df417c2ec6fe40c52865af70d7d9fea11cd36
Mcamargo85 <38304139+Mcamargo85@users.noreply.github.com>
2021-11-17 10:39:12 +0200

    Update slurm_multiprocess.py

--- utils/slurm_multiprocess.py
+++ utils/slurm_multiprocess.py
@@ -105,6 +105,7 @@ class HPC_Multiprocess():
                    'module load cuda/10.0',
                    'module load python/3.6.3/virtenv',
                    'module load java-1.8.0_40',
+                   'source deactivate',
                    'source activate ' + self.conn['env']
                    ]
         def format_option(short, parm):
