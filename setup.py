763337eb2d9d38d66d70428f2bcdb2fea8b94c88
Mcamargo85 <manuel.camargo@gmail.com>
2023-03-31 11:08:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='support_modules',
       description='support module with utility functions',
       author='Manuel Camargo',
       url='https://github.com/Mcamargo85/support_modules',
-      packages=find_packages(where='src'),
+      packages=['src.utils', 'src.readers', 'src.tests', 'src.analyzers'],
       install_requires=[
         'numpy',
         'networkx',
