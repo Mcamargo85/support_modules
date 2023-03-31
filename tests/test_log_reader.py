5b174c8df8c6427dfe12d6abe20bbc6da6fc2510
Mcamargo85 <manuel.camargo@gmail.com>
2023-03-31 11:53:00 +0300

    test: addition of test case

--- tests/test_log_reader.py
+++ tests/test_log_reader.py
@@ -1,13 +1,18 @@
 import os
 from unittest import TestCase
 from src.readers.log_reader import LogReader
+from parameterized import parameterized
 
 import pandas as pd
 
 
 class TestLogReader(TestCase):
 
-    def test_load_data_from_file(self):
+    @parameterized.expand([
+        ['purchasing_example', 'PurchasingExample.xes', 5, 10335],
+        ['confidential_1000', 'confidential_1000.xes', 5, 26886],
+    ])
+    def test_load_data_from_file(self, _name, event_log, cols, rows):
         # Event log reading
         column_names = {'Case ID': 'caseid', 'Activity': 'task',
                         'lifecycle:transition': 'event_type', 'Resource': 'user'}
@@ -16,8 +21,8 @@ class TestLogReader(TestCase):
                     'one_timestamp': False,
                     'filter_d_attrib': True}
 
-        log = LogReader(os.path.join('tests', 'fixtures', 'PurchasingExample.xes'), settings)
+        log = LogReader(os.path.join('tests', 'fixtures', event_log), settings)
         df = pd.DataFrame(log.data)
-        self.assertEqual(len(df.columns), 5)
-        self.assertEqual(len(df), 10335)
+        self.assertEqual(len(df.columns), cols)
+        self.assertEqual(len(df), rows)
 
